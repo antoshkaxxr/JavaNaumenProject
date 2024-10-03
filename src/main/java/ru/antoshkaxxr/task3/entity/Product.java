dd099e84255d7554d362977e020117b8ff2882cb
antoshkaxxr <gbanton2004@yandex.ru>
2024-10-03 13:58:07 +0500

    Initial commit

---
+++ src/main/java/ru/antoshkaxxr/task3/entity/Product.java
@@ -0,0 +1,31 @@
+package ru.antoshkaxxr.task3.entity;
+
+public class Product {
+    private Long id;
+    private String name;
+    private Integer caloriesNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCaloriesNumber() {
+        return caloriesNumber;
+    }
+
+    public void setCaloriesNumber(Integer caloriesNumber) {
+        this.caloriesNumber = caloriesNumber;
+    }
+}
