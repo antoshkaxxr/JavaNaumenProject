1125c539dd236aeacdc2625a55bb56010df8e1e4
everlast1ngw1nter <at07012004@gmail.com>
2024-11-15 20:45:56 +0500

    base redirect

--- src/main/java/ru/antoshkaxxr/JavaNaumenProject/Controllers/CalculatorController.java
+++ src/main/java/ru/antoshkaxxr/JavaNaumenProject/Controllers/CalculatorController.java
@@ -1,10 +1,14 @@
 package ru.antoshkaxxr.JavaNaumenProject.Controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Map;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
 import ru.antoshkaxxr.JavaNaumenProject.Dto.CalculatorData;
 
 @Controller
@@ -16,9 +20,27 @@ public class CalculatorController {
         return "calculatorForm";
     }
 
-    @ResponseBody
+
     @PostMapping("/evaluate")
-    public String returnCalculatorForm(CalculatorData data) {
-        return data.toString();
+    public String returnCalculatorForm(CalculatorData data,
+                                       RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("calculatorData", data);
+        return "redirect:/calculator/results";
+    }
+
+    @GetMapping("/results")
+    public String returnResults(HttpServletRequest request,
+                                Model model) {
+        Map<String, ?> inputFlashMap =
+                RequestContextUtils.getInputFlashMap(request);
+        if (inputFlashMap == null) {
+            return "redirect:/calculator";
+        }
+        var newBlog = (CalculatorData) inputFlashMap.get("calculatorData");
+        model.addAttribute("height", newBlog.height());
+        model.addAttribute("weight", newBlog.weight());
+        model.addAttribute("sex", newBlog.sex());
+        model.addAttribute("type", newBlog.formula());
+        return "calculationResults";
     }
 }
